c2301d2a3742014757c748babe58497eff80298a
Francesc Vila <francescvilasubias@gmail.com>
2023-05-22 11:47:16 +0200

    feat: normalize search filter strings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,14 @@ import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
 
 const filteredResources = (resources: Array<Resource>, filter: string) => {
-  return resources.filter((r) => (r.title.toLowerCase().includes(filter.toLowerCase()) || r.dimension.toLowerCase().includes(filter.toLowerCase())));
+  const curatedFilter = filter.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").trim();
+  
+  return resources.filter((r) => {
+    const title = r.title.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").trim();
+    const dimension = r.dimension.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").trim();
+
+    return title.includes(curatedFilter) || dimension.includes(curatedFilter);
+  });
 }
 
 const App = () => {
