7205bff4aa53108d130b2947659c631a42486bf3
Francesc Vila <francescvilasubias@gmail.com>
2023-05-22 09:15:35 +0200

    feat: logo and header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,13 @@ const App = () => {
 
   return (
     <main className='flex flex-col items-center w-screen h-screen'>
-      <header className="flex justify-center w-full py-2 text-2xl font-bold text-white bg-slate-400">Repositori CompREd</header>
+      <header className="flex flex-col items-center justify-center w-full px-2 pt-4 sm:gap-2 sm:justify-start sm:flex-row">
+        <span className="text-3xl font-semibold">Repositori</span>
+        <a href="https://webs.uab.cat/compred/" target="_blank" rel="noreferrer">
+          <img className="w-48" src="/logo.png" alt="logo" />
+        </a>
+        <span className="font-semibold font-xl">(work in progress)</span>
+      </header>
       
       <DimensionFilters setFilter={setFilter} />
 
